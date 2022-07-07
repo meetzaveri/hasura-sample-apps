44a9159255a7bb4153c34a1a276c754b4aab6ab9
meetzaveri <meethpatel96@gmail.com>
2022-07-07 19:54:15 +0530

    update: update default migration file

--- hasura-data/migrations/default/1657199741808_init/up.sql
+++ hasura-data/migrations/default/1657199741808_init/up.sql
@@ -14,3 +14,20 @@ ALTER SEQUENCE public.test_public_pg_id_seq OWNED BY public.test_public_pg.id;
 ALTER TABLE ONLY public.test_public_pg ALTER COLUMN id SET DEFAULT nextval('public.test_public_pg_id_seq'::regclass);
 ALTER TABLE ONLY public.test_public_pg
     ADD CONSTRAINT test_public_pg_pkey PRIMARY KEY (id);
+    CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    age numeric NOT NULL
+);
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
