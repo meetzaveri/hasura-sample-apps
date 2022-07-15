05b8893f37b55687526e8d1f4105aca76b0b4590
meetzaveri <meethpatel96@gmail.com>
2022-07-15 18:29:53 +0530

    update: update controller file and update deps

--- backend-server/controllers/index.js
+++ backend-server/controllers/index.js
@@ -1,72 +1,18 @@
 import nodemailer from "nodemailer";
 
-export const sendMail = async (req, res) => {
+export const getUserId = async (req, res) => {
   try {
-    console.log("req", req);
-    const { emailInput } = req.body.input;
-    //const inputDomain = emailInput.from.split('@')[1]
-
-    // Get the DKIM details from dailycloak
-    // const dkimDetails = await client.request(GET_SES_DOMAIN, {
-    //     domain: inputDomain,
-    // })
-
-    // if (dkimDetails.aws_ses.length === 0) {
-    //     return res.status(400).json({
-    //         success: false,
-    //         message: `Domain ${inputDomain} is not registered. Cannot send emails.`,
-    //     })
-    // }
-
-    // create nodemailer transport
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "meetzaveri96@gmail.com",
-        pass: "password",
-      },
-    });
-    // build and send the message
-    const message = {
-      from: emailInput.from,
-      to: emailInput.to,
-      subject: emailInput.subject,
-      html: emailInput.html,
-      attachments: "",
-    };
-
-    await transportEmail(transporter, message);
-    // if (dkimDetails.aws_ses[0].isVerified === true) {
-    //     await transportEmail(transport, message)
-    // } else {
-    //     throw new Error(
-    //         `Domain - ${inputDomain} - is not verified. Cannot send emails.`
-    //     )
-    // }
+    console.log("req.body", req.body);
 
     return res.status(200).json({
-      success: true,
-      message: "Email sent successfully!",
+      user_id: 2,
+      message: "Success",
     });
   } catch (error) {
     console.log(error);
     return res.status(400).json({
-      success: false,
-      message: error.message,
+      id: 0,
+      message: "Failure",
     });
   }
-};
-
-const transportEmail = async (transporter, message) => {
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(message, (err, info) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(info);
-      }
-    });
-  });
 };
--- backend-server/main.js
+++ backend-server/main.js
@@ -1,14 +1,25 @@
-import { sendMail } from "./controllers";
+import { getUserId } from "./controllers";
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+// import { createUser } from './controllers/hasuraCreateUser'
+
+app.use(cors());
+
+app.use(express.json());
+const router = express.Router();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "./"));
 app.get("/", (req, res) => {
   res.send("Hello World (from hasura backend) !");
 });
 
-app.post("/sendMail", sendMail);
+app.post("/getUserId", getUserId);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
